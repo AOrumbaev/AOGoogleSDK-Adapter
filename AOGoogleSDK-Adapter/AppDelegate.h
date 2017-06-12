e0ec9ceb8ffefd4abd538df7e5d6e9d72b488d92
Altynbek Orumbayev <stay.in.there@gmail.com>
2017-06-13 01:44:48 +0600

    Initial Commit

---
+++ AOGoogleSDK-Adapter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AOGoogleSDK-Adapter
+//
+//  Created by Altynbek Orumbayev on 6/13/17.
+//  Copyright © 2017 Altynbek Orumbayev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AOGoogleSDK-Adapter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AOGoogleSDK-Adapter
+//
+//  Created by Altynbek Orumbayev on 6/13/17.
+//  Copyright © 2017 Altynbek Orumbayev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
